7c4b9c095229bcea402244f5d665adb6185ce79b
icestormikk <jigalow.pavel2015@yandex.ru>
2024-04-06 21:19:59 +0300

    an abstract class has been added to add a unique id to the inherited classes

--- src/main/kotlin/common/Identifiable.kt
+++ src/main/kotlin/common/Identifiable.kt
@@ -1,4 +1,11 @@
 package common
 
-interface Identifiable {
+import kotlinx.serialization.Serializable
+import utils.serializers.UUIDSerializer
+import java.util.UUID
+
+@Serializable
+abstract class Identifiable {
+    @Serializable(UUIDSerializer::class)
+    val id: UUID = UUID.randomUUID()
 }
