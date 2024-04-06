0c149b600154c68ce15d7f45bb4408f44bafbc3f
icestormikk <jigalow.pavel2015@yandex.ru>
2024-04-07 00:05:41 +0300

    creating a class (singleton) containing operations on files

---
+++ src/main/kotlin/utils/helpers/FileHelper.kt
@@ -0,0 +1,29 @@
+package utils.helpers
+
+import java.io.File
+
+object FileHelper {
+    fun writeTo(filepath: String, content: String) {
+        try {
+            val file = File(filepath)
+
+            val buffer = file.bufferedWriter()
+            with(buffer) {
+                write(content)
+                close()
+            }
+        } catch (ex: Error) {
+            throw Error("Error while writing to $filepath: ${ex.message}")
+        }
+    }
+
+    fun <T> readFrom(filepath: String, onTransform: (content: String) -> T): T {
+        val file = File(filepath)
+
+        val stream = file.inputStream()
+        val content = stream.readBytes().toString(Charsets.UTF_8)
+
+        return onTransform(content)
+    }
+
+}
