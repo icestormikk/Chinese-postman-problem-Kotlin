d9c25109a106a1ce9a1725ca55a770fd3dcac397
icestormikk <jigalow.pavel2015@yandex.ru>
2024-04-07 00:06:31 +0300

    changing the identifier type of each entity

--- src/main/kotlin/common/Identifiable.kt
+++ src/main/kotlin/common/Identifiable.kt
@@ -1,11 +1,9 @@
 package common
 
 import kotlinx.serialization.Serializable
-import utils.serializers.UUIDSerializer
 import java.util.UUID
 
 @Serializable
 abstract class Identifiable {
-    @Serializable(UUIDSerializer::class)
-    val id: UUID = UUID.randomUUID()
+    val id: String = UUID.randomUUID().toString()
 }
