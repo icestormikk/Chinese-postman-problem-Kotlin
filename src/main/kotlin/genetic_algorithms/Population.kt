75927b5c5978b7148f557015b4135438218b8af8
icestormikk <jigalow.pavel2015@yandex.ru>
2024-04-15 23:00:44 +0300

    removed the check for the same length for the chromosomes of all entities in the population

--- src/main/kotlin/genetic_algorithms/Population.kt
+++ src/main/kotlin/genetic_algorithms/Population.kt
@@ -4,8 +4,4 @@ import common.Identifiable
 
 data class Population<T>(
     val entities: MutableList<Chromosome<T>>,
-) : Identifiable() {
-    init {
-        require(entities.all { it.genes.size == entities[0].genes.size }) { "All chromosomes must be the same length" }
-    }
-}
+) : Identifiable()
