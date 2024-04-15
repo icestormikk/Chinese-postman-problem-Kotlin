07c43d21ccaa5ab34407b47eed52c64dbc7af648
icestormikk <jigalow.pavel2015@yandex.ru>
2024-04-15 23:04:21 +0300

    again, a bit of random code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,70 @@
+import common.AlgorithmType
+import genetic_algorithms.Chromosome
+import genetic_algorithms.geneticAlgorithm
+import genetic_algorithms.outputToFile
 import graph.Edge
 import graph.Graph
 import graph.Node
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.encodeToString
+import utils.helpers.CommandLineHelper
 import utils.helpers.FileHelper
+import java.io.FileNotFoundException
+import java.nio.file.Files
+import kotlin.io.path.Path
 
-class DoubleGraph(
-    override val nodes: List<Node>,
-    override val edges: List<Edge<Double>>
-) : Graph<Double>(nodes, edges) {
-    override fun calculateTotalLengthOf(path: List<Edge<Double>>): Double {
+class DoubleGraph(nodes: List<Node>, edges: MutableList<Edge<Double>>): Graph<Double>(nodes, edges) {
+    override fun calculateTotalLengthOf(path: Array<Edge<Double>>): Double {
+        for (index in 0..<path.lastIndex) {
+            if (path[index].destination.id != path[index + 1].source.id) {
+                return Double.MAX_VALUE
+            }
+        }
         return path.sumOf { it.weight }
     }
 }
 
+@Serializable
+data class GraphDao(
+    val nodes: List<Node>,
+    val edges: List<Edge<Double>>
+) {
+    fun toDoubleGraph(): DoubleGraph {
+        return DoubleGraph(nodes, edges.toMutableList())
+    }
+}
 
+fun main(args: Array<String>) {
+    val arguments = args.toList().chunked(2).associate { it[0] to it[1] }
 
-fun main() {
-    val nodes = FileHelper.readFrom("C:\\Users\\jigal\\IdeaProjects\\chinese_postman_problem\\test.txt") {
-        Json.decodeFromString<List<Node>>(it)
+    val graphInputFilepath = CommandLineHelper.fetchArgument(arguments, "-graph-input-file") {
+        if (!Files.exists(Path(it))) {
+            throw FileNotFoundException("The file '$it' does not exist.")
+        }
+        it
+    }
+    val pathOutputFilepath = CommandLineHelper.fetchArgument(arguments, "-path-output-file") { it }
+    val algorithmType = CommandLineHelper.fetchArgument(arguments, "-algorithm-type") {
+        AlgorithmType.valueOf(it)
     }
 
-    println(nodes)
-//    val edges = listOf(
-//        Edge(nodes[0], nodes[1], Random.nextDouble(0.0, 100.0), EdgeType.DIRECTED),
-//        Edge(nodes[1], nodes[2], Random.nextDouble(0.0, 100.0), EdgeType.DIRECTED),
-//        Edge(nodes[2], nodes[0], Random.nextDouble(0.0, 100.0), EdgeType.DIRECTED),
-//    )
-//    val graph = DoubleGraph(nodes, edges)
-//
-//    val path = graph.getRandomPath(nodes[0])
-//    println(path.joinToString(", ") { "${it.source.label} -> ${it.destination.label}" })
-//    println(graph.calculateTotalLengthOf(path))
+    val graph = FileHelper.readFrom(graphInputFilepath) {
+        Json.decodeFromString<GraphDao>(it).toDoubleGraph()
+    }
+    val onFitness = { chromosome: Chromosome<Edge<Double>> ->
+        if (!chromosome.genes.all { graph.edges.any { edge -> edge.id == it.id } }) {
+            Double.MIN_VALUE
+        }
+        (-1) * graph.calculateTotalLengthOf(chromosome.genes)
+    }
+
+    when (algorithmType) {
+        AlgorithmType.GENETIC -> {
+            val result = geneticAlgorithm(graph, 1000, 1000, onFitness = onFitness)
+            outputToFile(pathOutputFilepath, result, onFitness) { graph.calculateTotalLengthOf(it) }
+        }
+        AlgorithmType.PARTICLE_SWARM -> TODO()
+        AlgorithmType.ANNEALING -> TODO()
+    }
 }
 
