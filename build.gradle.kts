9021e8b7640724fcad8a370f1ab3a0e4b5370305
icestormikk <jigalow.pavel2015@yandex.ru>
2024-04-15 22:52:57 +0300

    added a dependency for using logging tools in project files

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,10 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    implementation("ch.qos.logback:logback-classic:1.5.3")
+
     testImplementation(kotlin("test"))
 }
 
