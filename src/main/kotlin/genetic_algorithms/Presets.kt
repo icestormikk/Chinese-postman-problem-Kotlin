71c4252bc5d74f120d415f2d23b067974f012da1
icestormikk <jigalow.pavel2015@yandex.ru>
2024-04-24 22:00:19 +0300

    updating the algorithm for launching the particle swarm method

--- src/main/kotlin/genetic_algorithms/Presets.kt
+++ src/main/kotlin/genetic_algorithms/Presets.kt
@@ -1,54 +1,100 @@
 package genetic_algorithms
 
-import genetic_algorithms.operators.ParentSelectionMethods
-import genetic_algorithms.operators.RecombinationMethods
+import genetic_algorithms.operators.*
 import graph.Edge
 import graph.Graph
 import graph.Node
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.encodeToString
-import utils.helpers.FileHelper
-
-@Serializable
-data class PathDao(
-    val score: Double,
-    val edges: List<Edge<Double>>
-)
 
 fun <T> geneticAlgorithm(
     graph: Graph<T>,
-    iterationCount: Int,
-    populationSize: Int,
     onFitness: (chromosome: Chromosome<Edge<T>>) -> Double,
-    startNode: Node = graph.nodes.first(),
+    onDistance: (Chromosome<Edge<T>>, Chromosome<Edge<T>>) -> Double,
+    configuration: GeneticAlgorithmConfiguration
 ): Population<Edge<T>> {
+    val (
+        iterationCount,
+        populationSize,
+        start,
+        parentsConf,
+        recombinationType,
+        mutation,
+        newPopulationConf,
+    ) = configuration
+
+    val startNode = if (start == null) {
+        graph.nodes.random()
+    } else {
+        graph.getNodeById(start) ?: graph.nodes.random()
+    }
+
     val paths = MutableList(populationSize) { Chromosome(graph.getRandomPath(startNode).toTypedArray()) }
-    val population = Population(paths)
+    var population = Population(paths)
 
     for (i in 0..iterationCount) {
-        val parents = ParentSelectionMethods.panmixia(population)
-        val offspring = RecombinationMethods.singlePointCrossover(parents.first, parents.second)
+        val allParents = when (parentsConf.selection) {
+            SelectionMethods.Types.TOURNAMENT -> {
+                SelectionMethods.tournamentSelection(population, onFitness)
+            }
+            SelectionMethods.Types.ROULETTE_WHEEL -> {
+                SelectionMethods.rouletteWheelSelection(population, onFitness)
+            }
+        }
+        val selectedParents = when (parentsConf.chooser) {
+            ParentSelectionMethods.Types.PANMIXIA -> {
+                ParentSelectionMethods.panmixia(allParents)
+            }
+            ParentSelectionMethods.Types.INBREEDING -> {
+                ParentSelectionMethods.inbreeding(allParents, onDistance)
+            }
+            ParentSelectionMethods.Types.OUTBREEDING -> {
+                ParentSelectionMethods.outbreeding(allParents, onDistance)
+            }
+        }
+        val offspring = when (recombinationType) {
+            RecombinationMethods.Types.DISCRETE -> {
+                RecombinationMethods.discreteRecombination(selectedParents.first, selectedParents.second)
+            }
+            RecombinationMethods.Types.TWO_POINT_CROSSOVER -> {
+                RecombinationMethods.twoPointCrossover(selectedParents.first, selectedParents.second)
+            }
+            RecombinationMethods.Types.SINGLE_POINT_CROSSOVER -> {
+                RecombinationMethods.singlePointCrossover(selectedParents.first, selectedParents.second)
+            }
+            RecombinationMethods.Types.SHUFFLE -> {
+                RecombinationMethods.shuffleCrossover(selectedParents.first, selectedParents.second)
+            }
+        }
+        if (Math.random() > mutation.percent) {
+            offspring.toList().forEach {
+                when (mutation.type) {
+                    MutationMethods.Types.REPLACING -> {
+                        MutationMethods.replacingMutation(it, graph.edges.toTypedArray())
+                    }
+                    MutationMethods.Types.SWAPPING -> {
+                        MutationMethods.swappingMutation(it)
+                    }
+                }
+            }
+        }
 
-        with (population.entities) {
-            sortByDescending(onFitness)
-            this[lastIndex] = offspring.first
-            this[lastIndex - 1] = offspring.second
+        population.entities.addAll(offspring.toList())
+
+        population = when (newPopulationConf.type) {
+            NewPopulationMethods.Types.TRUNCATION -> {
+                NewPopulationMethods.truncationSelection(population, onFitness, newPopulationConf.rate)
+            }
+            NewPopulationMethods.Types.ELITE -> {
+                NewPopulationMethods.eliteSelection(population, onFitness, newPopulationConf.rate)
+            }
+            NewPopulationMethods.Types.EXCLUSION -> {
+                NewPopulationMethods.exclusionSelection(population, onFitness)
+            }
         }
     }
 
     return population
 }
 
-fun outputToFile(
-    filepath: String,
-    population: Population<Edge<Double>>,
-    onFitness: (chromosome: Chromosome<Edge<Double>>) -> Double,
-    onDistance: (path: Array<Edge<Double>>) -> Double
-) {
-    population.entities.sortByDescending(onFitness)
-    val best = population.entities.first()
-    val pathDao = PathDao(onDistance(best.genes), best.genes.toList())
-
-    FileHelper.writeTo(filepath, Json.encodeToString(pathDao))
+private fun <T> Graph<T>.getNodeById(id: String): Node? {
+    return nodes.find { it.id == id }
 }
