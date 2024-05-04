b69a68f82dfd1d8f2c119b2d40e01f9e84156413
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-04 14:16:04 +0300

    Добавил поле, которое будет содержать в себе конфигурацию для третьего метода - метода отжига

--- src/main/kotlin/common/Configuration.kt
+++ src/main/kotlin/common/Configuration.kt
@@ -3,11 +3,13 @@ package common
 import genetic_algorithms.GeneticAlgorithmConfiguration
 import kotlinx.serialization.Serializable
 import particles_swarm.ParticlesSwarmConfiguration
+import simulated_annealing.SimulatedAnnealingConfiguration
 
 @Serializable
 data class Configuration(
     val type: AlgorithmType,
     val genetic: GeneticAlgorithmConfiguration? = null,
-    val particleSwarm: ParticlesSwarmConfiguration? = null
+    val particleSwarm: ParticlesSwarmConfiguration? = null,
+    val annealing: SimulatedAnnealingConfiguration? = null,
 )
 
