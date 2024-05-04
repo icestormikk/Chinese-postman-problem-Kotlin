5ceb1da8387e708eacb88328a8432401b50f1afb
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-04 18:14:23 +0300

    Добавил название для аргумента, который содержит путь к файлу для записи результата

--- src/main/kotlin/utils/constants/CommandLineParameters.kt
+++ src/main/kotlin/utils/constants/CommandLineParameters.kt
@@ -2,5 +2,6 @@ package utils.constants
 
 const val CONFIGURATION_FILE_ARGUMENT = "--config"
 const val GRAPH_FILE_ARGUMENT = "--graph"
+const val RESULT_FILE_ARGUMENT = "--output"
 
 const val LOGGER_FILE_VM_OPTION = "logfile-path"
