eedfbe1dd114695deef20ea19ecdd4d493daca9a
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-05 14:29:46 +0300

    Сделал классы для вершин и рёбер графа незавивисимыми от класса Identifiable, чтобы id объектов не переопределялись

--- src/main/kotlin/graph/Edge.kt
+++ src/main/kotlin/graph/Edge.kt
@@ -1,13 +1,14 @@
 package graph
 
-import common.Identifiable
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 data class Edge<T>(
     val source: Node,
     val destination: Node,
     val weight: T,
-    val type: EdgeType
-) : Identifiable()
+    val type: EdgeType,
+    val id: String = UUID.randomUUID().toString()
+)
 
--- src/main/kotlin/graph/Node.kt
+++ src/main/kotlin/graph/Node.kt
@@ -1,12 +1,13 @@
 package graph
 
-import common.Identifiable
 import kotlinx.serialization.EncodeDefault
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 data class Node @OptIn(ExperimentalSerializationApi::class) constructor(
     @EncodeDefault(EncodeDefault.Mode.ALWAYS)
-    val label: String = "Node"
-) : Identifiable()
+    val label: String = "Node",
+    val id: String = UUID.randomUUID().toString()
+)
