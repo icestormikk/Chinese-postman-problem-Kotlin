7d20e225d775eb8a43d262536b79a1d0a11ac19a
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-10 18:51:06 +0300

    Убрал из ребра поле EdgeType

--- src/main/kotlin/graph/Edge.kt
+++ src/main/kotlin/graph/Edge.kt
@@ -1,14 +1,12 @@
 package graph
 
 import kotlinx.serialization.Serializable
-import java.util.UUID
 
 @Serializable
 data class Edge<T>(
     val source: Node,
     val destination: Node,
     val weight: T,
-    val type: EdgeType,
-    val id: String = UUID.randomUUID().toString()
+    val id: String
 )
 
