9ebe4347f5b32df36e7c537af18ba29bfae59e4e
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-11 18:22:34 +0300

    Добавил реализацию алгоритмов для чтения конфигурации, переданной пользователем, и работы программы в соответствии с ней

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,24 @@
 import common.AlgorithmType
 import common.Configuration
-import genetic_algorithms.geneticAlgorithm
+import common.Response
+import genetic_algorithms.Chromosome
+import genetic_algorithms.GeneticAlgorithm
 import graph.Node
 import graph.Edge
 import graph.Graph
 import graph.GraphDao
-import particles_swarm.particlesSwarm
+import particles_swarm.ParticleSwarm
+import simulated_annealing.SimulatedAnnealing
 import utils.constants.CONFIGURATION_FILE_ARGUMENT
-import utils.constants.GRAPH_FILE_ARGUMENT
 import utils.helpers.CommandLineHelper
 import utils.helpers.FileHelper
 import utils.helpers.JSONHelper
 import utils.helpers.LoggingHelper
-import utils.validators.ConfigurationValidator
-import utils.validators.GraphValidator
+import utils.constants.GRAPH_FILE_ARGUMENT
+import utils.constants.LOGGER_FILE_VM_OPTION
+import utils.constants.RESULT_FILE_ARGUMENT
+import kotlinx.serialization.encodeToString
+import java.nio.file.Paths
 
 class DoubleGraph(nodes: List<Node>, edges: MutableList<Edge<Double>>): Graph<Double>(nodes, edges) {
     override fun calculateTotalLengthOf(path: Array<Edge<Double>>): Double {
@@ -27,46 +32,93 @@ class DoubleGraph(nodes: List<Node>, edges: MutableList<Edge<Double>>): Graph<Do
 }
 fun GraphDao.toDoubleGraph() = DoubleGraph(nodes, edges.toMutableList())
 
-private val logger = LoggingHelper.getLogger("MAIN_LOGGER")
+private fun getDefaultLoggerFilepath() = Paths
+    .get(System.getProperty("user.home"), "chinese-postman-problem-program.log")
+    .toAbsolutePath()
+    .toString()
+
+private val logger = LoggingHelper().getLogger()
 
 fun main(args: Array<String>) {
-    val arguments = args.toList().chunked(2).associate { it[0] to it[1] }
-
-    val graphDao = CommandLineHelper.fetchArgument(arguments, GRAPH_FILE_ARGUMENT) {
-        FileHelper.readFrom(it) { content -> JSONHelper.getInstance().decodeFromString<GraphDao>(content) }
+    getDefaultLoggerFilepath().let { filepath ->
+        if (System.getProperty(LOGGER_FILE_VM_OPTION) == null) {
+            System.setProperty(LOGGER_FILE_VM_OPTION, filepath)
+            logger.info { "The user path to the log files is not specified. The standard path is used: ${getDefaultLoggerFilepath()}" }
+        } else {
+            logger.info { "A custom log path is used: ${System.getProperty(LOGGER_FILE_VM_OPTION)}" }
+        }
     }
-    GraphValidator.validateGraphDao(graphDao)
-    val graph = graphDao.toDoubleGraph()
 
-    val configuration = CommandLineHelper.fetchArgument(arguments, CONFIGURATION_FILE_ARGUMENT) {
-        FileHelper.readFrom(it) { content -> JSONHelper.getInstance().decodeFromString<Configuration>(content) }
-    }
-    ConfigurationValidator.validateConfiguration(configuration, graph)
+    try {
+        val arguments = args.toList().chunked(2).associate { it[0] to it[1] }
 
-    when (configuration.type) {
-        AlgorithmType.GENETIC -> {
-            logger.info { "Genetic Algorithm Started" }
-            geneticAlgorithm(
-                graph,
-                onFitness = { chromosome ->
-                    1 / graph.calculateTotalLengthOf(chromosome.genes)
-                },
-                onDistance = { chromosome, chromosome2 ->
-                    chromosome.genes.mapIndexed { index, edge -> if (edge.id != chromosome2.genes[index].id) 1 else 0 }
-                        .sum().toDouble()
-                },
-                configuration.genetic!!
-            ).also {
-                logger.info { "Genetic Algorithm Ended" }
+        val configuration = CommandLineHelper().fetchArgument(arguments, CONFIGURATION_FILE_ARGUMENT, true) {
+            FileHelper().readFrom<Configuration>(it) { content -> JSONHelper().getInstance().decodeFromString(content) }
+        }!!
+
+        val graphDao = CommandLineHelper().fetchArgument(arguments, GRAPH_FILE_ARGUMENT, true) {
+            FileHelper().readFrom<GraphDao>(it) { content -> JSONHelper().getInstance().decodeFromString(content) }
+        }!!
+        val graph = graphDao.toDoubleGraph()
+
+        val resultFilepath = CommandLineHelper().fetchArgument(arguments, RESULT_FILE_ARGUMENT, true) { it }!!
+
+        val response: Response = when (configuration.type) {
+            AlgorithmType.GENETIC -> {
+                if (configuration.genetic == null) {
+                    throw Exception("The configuration for the genetic algorithm was not passed")
+                }
+
+                fun onFitness(chromosome: Chromosome<Edge<Double>>): Double {
+                    for (edge in graph.edges) {
+                        if (!chromosome.genes.contains(edge)) {
+                            return Double.MIN_VALUE
+                        }
+                    }
+
+                    return (-1) * graph.calculateTotalLengthOf(chromosome.genes)
+                }
+                fun onDistance(ch: Chromosome<Edge<Double>>, ch2: Chromosome<Edge<Double>>): Double {
+                    return ch.genes.mapIndexed { index, edge -> if (edge.id != ch2.genes[index].id) 1 else 0 }.sum().toDouble()
+                }
+
+                val result = GeneticAlgorithm().start(
+                    graph,
+                    ::onFitness,
+                    { ch, ch2 -> onDistance(ch, ch2) },
+                    configuration.genetic
+                )
+                result.entities.sortByDescending(::onFitness)
+
+                println(graph.calculateTotalLengthOf(result.entities[0].genes))
+                Response(result.entities[0].genes.map { it.id })
+            }
+            AlgorithmType.PARTICLES_SWARM -> {
+                if (configuration.particleSwarm == null) {
+                    throw Exception("The configuration for the particle swarm method was not passed")
+                }
+                val result = ParticleSwarm().start(graph, configuration.particleSwarm)
+                Response(result.map { index -> graph.edges[index].id })
+            }
+            AlgorithmType.ANNEALING -> {
+                if (configuration.annealing == null) {
+                    throw Exception("The configuration for the annealing method was not passed")
+                }
+                val selectedNode = graph.nodes.find { it.id == configuration.annealing.selectedNodeId } ?: graph.nodes.random()
+                val result = SimulatedAnnealing().start(
+                    graph,
+                    configuration.annealing,
+                    { state -> graph.calculateTotalLengthOf(state) },
+                    { graph.getRandomPath(selectedNode).toTypedArray() },
+                    selectedNode
+                )
+                Response(result.map { it.id })
             }
         }
-        AlgorithmType.PARTICLES_SWARM -> {
-            logger.info { "Particles Swarm Algorithm Started" }
-            particlesSwarm(graph, configuration.particleSwarm!!).also {
-                logger.info { "Particles Swarm Algorithm Ended" }
-            }
-        }
-        AlgorithmType.ANNEALING -> TODO()
+
+        FileHelper().writeTo(resultFilepath, JSONHelper().getInstance().encodeToString(response))
+    } catch (e: Exception) {
+        logger.error { e.message }
+        throw e
     }
 }
-
