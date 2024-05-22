dd63c6d00aca6a763b4df6c2903d23cd0048b3e1
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-22 17:48:55 +0300

    Удалил лишнее переопределение метода getEdgesFrom

--- src/main/kotlin/ant_colony/PheromoneGraph.kt
+++ src/main/kotlin/ant_colony/PheromoneGraph.kt
@@ -1,16 +1,8 @@
 package ant_colony
 
-import graph.EdgeType
 import graph.Graph
 import graph.Node
 
 abstract class PheromoneGraph<T>(
     nodes: List<Node>, override val edges: MutableList<PheromoneEdge<T>>
-): Graph<T, PheromoneEdge<T>>(nodes, edges) {
-    override fun getEdgesFrom(node: Node): List<PheromoneEdge<T>> {
-        return edges.filter {
-            (it.type == EdgeType.DIRECTED && it.source.id == node.id) ||
-                    (it.type == EdgeType.NOT_ORIENTED && (it.source.id == node.id || it.destination.id == node.id))
-        }
-    }
-}
+): Graph<T, PheromoneEdge<T>>(nodes, edges)
