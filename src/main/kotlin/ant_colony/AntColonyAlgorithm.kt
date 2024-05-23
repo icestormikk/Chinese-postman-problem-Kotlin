665c632a763dd3d9fa71b38ffa79d5a77ab2e709
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-23 13:55:58 +0300

    Добавил поясняющие комментарии

--- src/main/kotlin/ant_colony/AntColonyAlgorithm.kt
+++ src/main/kotlin/ant_colony/AntColonyAlgorithm.kt
@@ -21,9 +21,11 @@ class AntColonyAlgorithm {
         graph: Graph<Double, Edge<Double>>,
         configuration: AntColonyAlgorithmConfiguration
     ): MutableList<PheromoneEdge<Double>> {
+        // определяем начальные значения параметров
         val (
             iterationCount, antCount, startPheromoneValue, proximityCoefficient, alpha, beta, remainingPheromoneRate, q, startNodeId
         ) = configuration
+        // определяем стартовую вершину
         val startNode = if (startNodeId == null) {
             graph.nodes.random()
         } else {
@@ -31,31 +33,38 @@ class AntColonyAlgorithm {
         }
 
         logger.info { "Adding pheromones on the edge of the graph (initial value: ${startPheromoneValue})" }
+        // преобразуем исходный граф, полученный от пользователя в граф, содержащий феромоны
         val phGraph = PheromoneDoubleGraph(
             graph.nodes,
             graph.edges.map { PheromoneEdge(it.id, it.source, it.destination, it.weight, it.type, startPheromoneValue) }.toMutableList()
         )
 
+        // создаём переменные для хранения лучшего (кратчайшего) пути и его длины
         var bestPath = mutableListOf<PheromoneEdge<Double>>()
         var bestLength = Double.MAX_VALUE
 
         logger.info { "An algorithm for simulating an ant colony is launched ($iterationCount iterations, $antCount ants)" }
+        // пока не будет достигнуто максимальное количество итераций
         for (iteration in 1..iterationCount) {
-            phGraph.edges.forEach { it.pheromoneCount * remainingPheromoneRate }
             // феромоны на путях испаряются
+            phGraph.edges.forEach { it.pheromoneCount *= remainingPheromoneRate }
 
-            for (antIndex in 1..antCount) { val ant = Ant("Ant-${antIndex}")
+            // для каждого муравья
             for (antIndex in 1..antCount) {
                 val ant = Ant("Ant-${antIndex}")
+                // получаем путь, по которому прошёл муравей
                 val path = ant.getPath(phGraph, startNode, proximityCoefficient, alpha, beta)
+                // вычисляем его длину
                 val length = phGraph.calculateTotalLengthOf(path.toTypedArray())
 
+                // сравниваем с лучшим путём и при необходимости обновляем его
                 if (length < bestLength) {
                     logger.info { "The minimum path has been updated!\tIteration number: $iteration, length: $length" }
                     bestLength = length
                     bestPath = path
                 }
 
+                // обновляем количество феромонов на пройденный ребёр (добавка)
                 path.forEach { edge ->
                     val indexInGraph = phGraph.edges.indexOfFirst { it.id == edge.id }
                     val newPheromoneCount = q / length
