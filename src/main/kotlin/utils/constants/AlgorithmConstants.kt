903dace4b51f1eab8f38d59d576bf86fb659a503
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-27 15:38:30 +0300

    Добавил новые константы

---
+++ src/main/kotlin/utils/constants/AlgorithmConstants.kt
@@ -0,0 +1,11 @@
+package utils.constants
+
+/**
+ * Константа, указывающая длину несуществующего в графе пути
+ */
+const val NOT_EXISTENT_PATH_VALUE = Double.MAX_VALUE
+
+/**
+ * Константа, обозначающая пригодность худшего решения
+ */
+const val WORST_SOLUTION_FITNESS_VALUE = Double.NEGATIVE_INFINITY
