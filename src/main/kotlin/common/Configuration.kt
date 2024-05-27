ff120b580289644a38aa179faaebd75baa798f57
icestormikk <jigalow.pavel2015@yandex.ru>
2024-05-27 15:39:35 +0300

    Добавил новое поле для хранения максимальной длины искомого пути

--- src/main/kotlin/common/Configuration.kt
+++ src/main/kotlin/common/Configuration.kt
@@ -7,12 +7,14 @@ import kotlinx.serialization.Serializable
 /**
  * Класс для хранения полученных от пользотеля данных о желаемом алгоритме и его начальных значениях
  * @property type Тип алгоритма, по которому будет рассчитываться оптимальный путь
+ * @property maxLength Максимальная длина искомого пути
  * @property genetic Конфигурация для генетического алгоритма
  * @property antColony Конфигурация для метода имитации муравьиной колонии
  */
 @Serializable
 data class Configuration(
     val type: AlgorithmType,
+    val maxLength: Double,
     val genetic: GeneticAlgorithmConfiguration? = null,
 //    val particleSwarm: ParticlesSwarmConfiguration? = null,
 //    val annealing: SimulatedAnnealingConfiguration? = null,
